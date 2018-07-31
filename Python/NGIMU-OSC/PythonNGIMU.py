f824413a8f428ab2513933cc99585f846466edc6
JesusPinedaC <jesusdpinedac@gmail.com>
2018-07-31 14:57:56 -0500

    allow labview and python communication and fix indent

--- Python/NGIMU-OSC/PythonNGIMU.py
+++ Python/NGIMU-OSC/PythonNGIMU.py
@@ -4,7 +4,7 @@ import numpy as np
 import nibabel
 
 def process_arguments(argv):
-	parser = argparse.ArgumentParser(description="NGIMU python example")
+	parser = argparse.ArgumentParser(description="NGIMU python-labVIEW")
 	parser.add_argument(
 		"--ip",
 		default="192.168.1.1",
@@ -26,7 +26,6 @@ def process_arguments(argv):
 
 	return parser.parse_args(argv[1:])
 
-
 #https://ubuntuforums.org/showthread.php?t=821325
 def get_address():
     try:
@@ -40,43 +39,56 @@ def get_address():
         s.connect(('4.2.2.1', 0))
         address = s.getsockname()[0]
     return address
-
+    
 def main(argv):
-	args = process_arguments(argv)
+    args = process_arguments(argv)
+    
+    sock_r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_r.connect(('127.0.0.1', 777))
+    
+    sock_p = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_p.connect(('127.0.0.1', 778))
+    
+    sock_y = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock_y.connect(('127.0.0.1', 779))
+    
+    # Set the NGIMU to send to this machine's IP address
+    client = udp_client.SimpleUDPClient(args.ip, args.port)
+    client.send_message('/wifi/send/ip', get_address())
+    client.send_message('/wifi/send/port', args.receive_port)
+	
 
-	# Set the NGIMU to send to this machine's IP address
-	client = udp_client.SimpleUDPClient(args.ip, args.port)
-	client.send_message('/wifi/send/ip', get_address())
-	client.send_message('/wifi/send/port', args.receive_port)
+    def quaternionHandler(add, x, y, z, w):
+	    angls = np.array(nibabel.eulerangles.quat2euler([w, x, y, z]))*180/np.pi
+	    roll = angls[0]; pitch = -1*angls[1];
+	    if(angls[2] < 0): yaw = 180+angls[2]
+	    else: yaw = -180+angls[2]
+	    print('[roll, pitch, yaw]: [{},{},{}]'.format(roll, pitch, yaw))
+	    sock_r.send(str.encode(str(roll)+'\r\n'))
+	    sock_p.send(str.encode(str(pitch)+'\r\n'))
+	    sock_y.send(str.encode(str(yaw)+'\r\n'))
+	    
+    dispatch = dispatcher.Dispatcher()
+    dispatch.map('/quaternion', quaternionHandler)
 
-	def quaternionHandler(add, x, y, z, w):
-		angls = np.array(nibabel.eulerangles.quat2euler([w, x, y, z]))*180/np.pi
-		roll = angls[0]; pitch = -1*angls[1];
-		if(angls[2] < 0): yaw = 180+angls[2]
-		else: yaw = -180+angls[2]
-		print('[roll, pitch, yaw]: [{},{},{}]'.format(roll, pitch, yaw))
+    # Set up receiver
+    receive_address = args.udp_ip_address, args.receive_port
+    server = osc_server.ThreadingOSCUDPServer(receive_address, dispatch)
 
-	dispatch = dispatcher.Dispatcher()
-	dispatch.map('/quaternion', quaternionHandler)
-
-	# Set up receiver
-	receive_address = args.udp_ip_address, args.receive_port
-	server = osc_server.ThreadingOSCUDPServer(receive_address, dispatch)
-
-	print("\nUse ctrl-C to quit.")
-	# Start OSCServer
-	server_thread = threading.Thread(target=server.serve_forever)
-	server_thread.start()
+    print("\nUse ctrl-C to quit.")
+    # Start OSCServer
+    server_thread = threading.Thread(target=server.serve_forever)
+    server_thread.start()
 
 	# Loop while threads are running
-	try :
-		while 1 :
-			time.sleep(10)
+    try :
+        while 1 :
+            time.sleep(10)
 
-	except KeyboardInterrupt :
-		server.shutdown()
-		server_thread.join()
-		return 0
+    except KeyboardInterrupt :
+        server.shutdown()
+        server_thread.join()
+        return 0
 
 if __name__ == '__main__':
- sys.exit(main(sys.argv))
+    sys.exit(main(sys.argv))
